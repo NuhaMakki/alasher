a12b09a8f8c29da79d0d3819f14ec71e62baa900
NuhaMakki <noha.m.makki@gmail.com>
2023-10-30 23:06:20 +0300

    deploy vercel

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Home.html');
+  res.render('Home.ejs');
 })
 
 app.listen(port, () => {
